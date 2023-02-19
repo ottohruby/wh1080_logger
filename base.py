d2c252aa425e7bdc4a53fbb41bcec540b695139d
Otto Hruby <xhruby27@vutbr.cz>
2023-02-19 22:43:03 +0100

    Moved settings to base

--- base.py
+++ base.py
@@ -1,2 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
+
+# todo move somewhere else, so it is not exposed
+settings = {"user": "root", 
+            "password": "raspberry",
+            "host": "127.0.0.1", #127.0.0.1
+            "port": "3306",
+            "default_database": "data"}
--- main.py
+++ main.py
@@ -3,16 +3,9 @@ import base
 import weather_model
 import weather
 
-# todo move somewhere else, so it is not exposed
-settings = {"user": "root", 
-            "password": "raspberry",
-            "host": "127.0.0.1", #127.0.0.1
-            "port": "3306",
-            "default_database": "data"}
-
 if __name__ == "__main__":
     # Define the MariaDB engine using MariaDB Connector/Python
-    engine = sqlalchemy.create_engine(f"""mariadb+mariadbconnector://{settings.get('user')}:{settings.get('password')}@{settings.get('host')}:{settings.get('port')}/{settings.get('default_database')}""")
+    engine = sqlalchemy.create_engine(f"""mariadb+mariadbconnector://{base.settings.get('user')}:{base.settings.get('password')}@{base.settings.get('host')}:{base.settings.get('port')}/{base.settings.get('default_database')}""")
 
     base.Base.metadata.create_all(engine, checkfirst=True)
     Session = sqlalchemy.orm.sessionmaker(bind=engine)
@@ -29,3 +22,4 @@ if __name__ == "__main__":
         # Reset counter in weatherstation if logged to db
         ws.prepare_set_data_count(0)
         ws.set_fixed_data()
+        
