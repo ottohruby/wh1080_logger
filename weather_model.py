0ee61d21816f8644370ab27a4aed454ca36cbe9e
Otto Hruby <xhruby27@vutbr.cz>
2023-02-19 22:45:59 +0100

    Added selectWeatherReplication for model

--- weather_model.py
+++ weather_model.py
@@ -50,4 +50,14 @@ def addWeather(session, rows):
       session.add(newWeather)
       
    session.commit()
+
+def selectWeatherReplication(session):
+   rows = session.query(Weather).filter_by(replicated=False).order_by(Weather.id.desc()).limit(2).all()
    
+   r = []
+   for row in rows:
+      d = dict(row.__dict__)
+      d.pop('_sa_instance_state', None)
+      r.append(d)
+   
+   return r
